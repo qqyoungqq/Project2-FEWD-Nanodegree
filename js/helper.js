f2f007704ea61991551f4049810e3600cfd1b93e
qqyoungqq <qianyang@mail.usf.edu>
2015-02-19 17:29:23 -0500

    Fix the bug in "the international name challenge" in lesson2, add input parameter oldname when calling function inName

--- js/helper.js
+++ js/helper.js
@@ -62,10 +62,14 @@ var googleMap = '<div id="map"></div>';
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
+
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+  var oldname = $('#name').html();
+  // or 
+  // var oldname = bio.name;
+  var iName = inName(oldname) || function(){};
+  $('#name').html(iName);
   });
 });
 
@@ -233,11 +237,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+   map.fitBounds(mapBounds);
+});
